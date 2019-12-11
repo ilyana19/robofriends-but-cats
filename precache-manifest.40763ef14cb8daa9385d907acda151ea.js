49bd4b91965d9b35f788890ccaa2f0fea35ece60
ilyana19 <aries_2152@hotmail.com>
2019-12-11 18:38:50 -0500

    Updates

---
+++ precache-manifest.40763ef14cb8daa9385d907acda151ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d366ebba7262bdbf69bacfc55a28c1",
+    "url": "/ilyana19/robofriends-but-cats/index.html"
+  },
+  {
+    "revision": "7eb3beebc5013af825e8",
+    "url": "/ilyana19/robofriends-but-cats/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a88b0daf52412bee02d8",
+    "url": "/ilyana19/robofriends-but-cats/static/css/main.8ed91307.chunk.css"
+  },
+  {
+    "revision": "7eb3beebc5013af825e8",
+    "url": "/ilyana19/robofriends-but-cats/static/js/2.908ef5fb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ilyana19/robofriends-but-cats/static/js/2.908ef5fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a88b0daf52412bee02d8",
+    "url": "/ilyana19/robofriends-but-cats/static/js/main.f5107673.chunk.js"
+  },
+  {
+    "revision": "fe5c37263b96c9250145",
+    "url": "/ilyana19/robofriends-but-cats/static/js/runtime-main.b07fca46.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ilyana19/robofriends-but-cats/static/media/SEGA.12a3db8b.woff"
+  }
+]);
