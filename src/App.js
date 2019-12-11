ae4e9d7fadc00db2ef9f6606a71b77912ce6a8bc
ilyana19 <aries_2152@hotmail.com>
2019-12-10 21:57:05 -0500

    started making the search box by keeping track of the state of the input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchField: '',
+    }
+  }
+
+  onSearchChange = (event) => {
+    console.log(event.target.value);
+    this.setState({ searchField: event.target.value });
+
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    });
+    console.log(filteredRobots);
+  }
+
+  render() {
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={this.state.robots} />
+      </div>
+    );
+  }
 }
 
 export default App;
