4d8faebfef3e97e34a7ff812ccb27a1e495a9fec
ilyana19 <aries_2152@hotmail.com>
2019-12-11 17:59:40 -0500

    made a scoll component wrapping cardlist

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Card = ({id, name, email}) => {
-
   return (
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
       <img src={`https://robohash.org/set_set4/${id}?size=200x200`} alt="cat" />
